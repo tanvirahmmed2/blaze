16bc0dccbf5fceb547b52b692b31b6efc457e53a
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-08-09 00:12:01 +0600

    update

--- server/src/middlesares/uploadFile.js
+++ server/src/middlesares/uploadFile.js
@@ -1,11 +1,16 @@
+require('dotenv').config()
 const multer= require("multer")
 const path= require('path')
-const { uploadFile } = require("../secret")
+createErr= require("http-errors")
+
+const UPLOAD_FILE= process.env.UPLOAD_FILE
+const FILE_TYPE= process.env.FILE_TYPE || ['jpg', 'jpeg', 'png']
+const MAX_FILE_SIZE= Number(process.env.MAX_FILE_SIZE) || 2097152
 
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, uploadFile)
+    cb(null, UPLOAD_FILE)
   },
   filename: function (req, file, cb) {
     const extname= path.extname(file.originalname)
@@ -13,11 +18,27 @@ const storage = multer.diskStorage({
   }
 })
 
+const fileFilter= (req,file,cb)=>{
+    const extname= path.extname(file.originalname)
+    if(!FILE_TYPE.includes(extname.substring(1))){
+      const error= createErr(404, 'file type not allowed')
+    
+      return cb(error)
+    }
+    console.log(FILE_TYPE)
+    cb(null, true)
+
+
+}
 
 
 
 
 
-const upload = multer({ storage: storage })
+const upload = multer({ storage: storage ,
+
+  limits: {fileSize: MAX_FILE_SIZE},
+  fileFilter
+})
 
 module.exports= {upload}
