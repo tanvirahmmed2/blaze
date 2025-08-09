46c757eafe7f057c70bf78f205a9e92c37d99bbe
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-08-09 23:42:26 +0600

    update

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,36 +1,51 @@
-const createErr= require("http-errors")
-const bcrypt= require('bcryptjs')
-const jwt= require('jsonwebtoken')
-const User= require("../models/user.model")
-const {createJsonwebtoken}= require("../helper/jsonwebtoken")
+const createErr = require("http-errors")
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const User = require("../models/user.model")
+const { createJsonwebtoken } = require("../helper/jsonwebtoken")
+const { jwtaccesskey } = require("../secret")
 
 
-const handleLogin= async (req,res)=>{
+
+const handleLogin = async (req, res) => {
     try {
         //email/password
-        const {email, password}= req.body
+        const { email, password } = req.body
         //isexist
-        const user= await User.findOne({email})
-        if(!user) throw createErr(404, 'user doesnot exist, please register first')
-        
+        const user = await User.findOne({ email })
+        if (!user) throw createErr(404, 'user doesnot exist, please register first')
+
         //password check
-        const isPasswordMatch= await bcrypt.compare(password, user.password)
-        if(!isPasswordMatch) throw createErr(404, "password wrong, try again")
-        if(user.isBanned) {
+        // const isPasswordMatch = await bcrypt.compare(password, user.password)
+        // if (!isPasswordMatch) throw createErr(404, "password wrong, try again")
+        if (user.isBanned) {
             throw createErr(403, "you're banned. Please contact authority")
         }
-        
-        
+        const accesstoken = createJsonwebtoken(
+            {email },
+            jwtaccesskey,
+            "10m"
+        );
+
+
+        res.cookie('acces_token', accesstoken, {
+            maxAge: 15*60*1000,
+            httpOnly: true,
+            secure: true,
+            sameSite: 'none',
+        })
+
+
 
         //success response
         return res.status(200).send({
             message: 'user logged in succefully',
-            payload: {user}
+            payload: { user }
         })
     } catch (error) {
-        
+
     }
 }
 
 
-module.exports= handleLogin
+module.exports = handleLogin
--- server/src/secret.js
+++ server/src/secret.js
@@ -4,9 +4,10 @@ const serverPort= process.env.PORT
 const mongodbUrl= process.env.MONGODB_URL || "mongodb://localhost:27017/ecommerceMernDB"
 const defaultImage= process.env.DEFAULT_IMAGE
 const jwtactivationkey= process.env.JWT_ACTIVATION_KEY || "ysiytg73768534wbv85t362bvg85ygt"
+const jwtaccesskey= process.env.JWT_ACCESS_KEY || "ysiytg73768534wbv85t362bvsdwg85ygt"
 const smtpUSERNAME= process.env.SMTP_USERNAME
 const smtpPASSWORD= process.env.SMTP_PASSWORD
 const clientURL= process.env.CLIENT_URL
 const uploadFile= process.env.UPLOAD_FILE
 
-module.exports= { serverPort, clientURL, mongodbUrl, defaultImage, jwtactivationkey, smtpPASSWORD, smtpUSERNAME, uploadFile}
+module.exports= { serverPort, clientURL, mongodbUrl, defaultImage, jwtactivationkey, smtpPASSWORD, smtpUSERNAME, uploadFile, jwtaccesskey}
