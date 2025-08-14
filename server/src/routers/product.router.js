aeceea7e636e2245758322cc9a7244d0e61cf696
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-08-15 02:13:54 +0600

    update

--- server/src/routers/product.router.js
+++ server/src/routers/product.router.js
@@ -2,11 +2,13 @@ const express= require('express')
 const {getProducts, handlecreateProduct} = require('../controllers/product.controller')
 const { isLoggedin, isAdmin } = require('../middlesares/auth')
 const { uploaduserImage } = require('../middlesares/uploadFile')
+const validateProduct = require('../validator/product')
+const { runValidation } = require('../validator')
 
 const productRouter= express.Router()
 
 productRouter.get('/',getProducts)
-productRouter.post('/create', isLoggedin, isAdmin, uploaduserImage.single('image'),handlecreateProduct)
+productRouter.post('/create', isLoggedin, isAdmin, uploaduserImage.single('image'), validateProduct, runValidation,handlecreateProduct)
 
 
 
