2976792a59ad825f2fdd4a71535fbb84808d5903
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-08-17 03:29:53 +0600

    update

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import About from './pages/About'
 import Cart from './pages/Cart'
 import Register from './pages/Register'
@@ -10,12 +10,12 @@ import Error from './pages/Error'
 const Router = () => {
   return (
     <Routes>
-        <Route path='/about' element={ <About/>}/>
-        <Route path='/register' element={ <Register/>}/>
-        <Route path='/login' element={ <Login/>}/>
-        <Route path='/cart' element={ <Cart/>}/>
-        <Route path='/*' element={ <Error/>}/>
-      </Routes>
+      <Route path='/about' element={<About />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/cart' element={<Cart />} />
+      <Route path='/*' element={<Error />} />
+    </Routes>
   )
 }
 
