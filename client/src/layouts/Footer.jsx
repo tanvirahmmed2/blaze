01fa5c99094839161aeac2881cc1230801a4d375
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-08-17 03:29:11 +0600

    update

--- client/src/layouts/Footer.jsx
+++ client/src/layouts/Footer.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <footer className='w-full bg-green-500 text-white py-6 flex flex-col md:flex-row gap-4 items-center justify-center'>
+        <form           className='w-auto rounded-xl flex flex-col items-center justify-center border-2  gap-2 p-4'>
+            <label htmlFor="subscribe" >subscribe</label>
+            <input type="text" name='subscribe' id='subscribe' placeholder='enter your email' className='border-2 outline-none py-2 px-4 rounded-lg'/>
+            <button type='submit' className='border-2 px-2 rounded-lg'>Subscribe</button>
+        </form>
+        <div>
+            <p className='w-auto flex flex-row'>Copyright by @<h1>Tanvir Ahmmed</h1></p>
+
+        </div>
+    </footer>
   )
 }
 
