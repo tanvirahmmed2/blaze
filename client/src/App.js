50d090c9fbfa83f33e36842d386930ed2778acf4
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-08-17 03:18:22 +0600

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import Navbar from './pages/Navbar'
+import Navbar from './layouts/Navbar'
 import Router from './Router'
+import Footer from './layouts/Footer'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Router/>
+      <Footer/>
     </div>
   )
 }
